eb78d738d7c150450d265c085750bcc7bf11d666
salauddin <snalband@thoughtworks.com>
2018-06-02 22:27:46 +0530

    Implicit intent

--- app/src/main/java/com/example/stammana/androidworkshop/HomeActivity.java
+++ app/src/main/java/com/example/stammana/androidworkshop/HomeActivity.java
@@ -1,10 +1,12 @@
 package com.example.stammana.androidworkshop;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,5 +17,16 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
- 
+    public void capture(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            ImageView imageView = findViewById(R.id.image);
+            imageView.setImageBitmap((Bitmap) data.getExtras().get("data"));
+        }
+    }
 }

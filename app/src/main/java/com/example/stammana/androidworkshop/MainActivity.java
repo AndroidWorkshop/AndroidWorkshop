a7a72ae3b1267324e8232930597d81f7b9afe968
salauddin <snalband@thoughtworks.com>
2018-06-02 22:17:09 +0530

    Explicit intent

--- app/src/main/java/com/example/stammana/androidworkshop/MainActivity.java
+++ app/src/main/java/com/example/stammana/androidworkshop/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.stammana.androidworkshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String USERNAME = "swecha";
+    private static final String PASSWORD = "swecha";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        TextView userNameView = (TextView) findViewById(R.id.username);
+        TextView passwordView = (TextView) findViewById(R.id.password);
+        String username = userNameView.getText().toString();
+        String password = passwordView.getText().toString();
+        Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+        if(username.equals(USERNAME) && password.equals(PASSWORD)){
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this,"Please Enter valid details",Toast.LENGTH_SHORT).show();
+        }
     }
 }
